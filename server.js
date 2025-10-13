7891cd9ee0a7035432f0658c5dbb48e83eaf952b
Samanth <gsamanth@mirabeltechnologies.com>
2025-10-13 13:40:50 +0530

    Handled ALB SSL certificate

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const redisClient = new Redis.Cluster(
   {
     dnsLookup: (address, callback) => callback(null, address),
     redisOptions: {
-      tls: {},
+      tls: {
+        rejectUnauthorized: false
+      },
     },
   });
 
