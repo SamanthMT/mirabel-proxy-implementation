08a3d8a589603ca58e7acc10eccf0bf4adf3027e
Samanth <gsamanth@mirabeltechnologies.com>
2025-10-13 20:33:45 +0530

    Fixed CORS issue

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, secret_key');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 const isProd = process.env.NODE_ENV === "production";
 
 const proxyMapFile = isProd ? "./proxy-map.prod.json" : "./proxy-map.dev.json";
